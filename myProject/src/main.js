3e4afd417f8f03fe69606e0d93021a8cabf5580e
zcy <208114298@qq.com>
2023-06-24 15:39:05 +0800

    Layout-静态资源初始化

--- myProject/src/main.js
+++ myProject/src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { getCategory } from './apis/testApi'
+//引入初始化样式文件
+import '@/styles/common.scss'
 getCategory().then(res=>{
     console.log(res)
 })
